6645ba7f1d5c5b72278f590518087eca90c5216a
AntzUhl <1325200471@qq.com>
2020-07-17 10:53:06 +0800

    change html parse lens

--- spider/spider/main.py
+++ spider/spider/main.py
@@ -16,9 +16,10 @@ i = 1
 while 1 :
     url = 'https://api.bilibili.com/medialist/gateway/base/spaceDetail?media_id=88854277&pn='+ str(i) +'&ps=20&keyword=&order=mtime&type=0&tid=0&jsonp=jsonp'
     html = requests.get(url)
+    print("page: ", i)
     i = i + 1
-    print(html.text)
     res = json.loads(html.text)
-    len_video = len(res['data']['medias'][0])
+    len_video = len(res['data']['medias'])
+    print("vide nums: ", len_video)
     for id in range(0,len_video):
         create_thread(res['data']['medias'][id])
